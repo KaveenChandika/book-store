d8ae7be517f032a695dca202d13214f54dd61bdf
Kaveen <kaveen@digitalxlabs.com>
2024-05-17 10:21:37 +0530

    default router changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client';
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-  return (
-    <div>
-    </div>
-  );
+  const router = useRouter();
+
+  useEffect(() => {
+    // Redirect to the specific page
+      router.replace('/Products');
+  }, [router]);
 }
